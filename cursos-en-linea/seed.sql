253ee78518741eb3831d08882a56c6ea470646cc
AaronLeon13 <aaronleon13@gmail.com>
2026-03-23 16:22:10 -0700

    feat: Add initial database schema, seed data, and join query examples to `seed.sql`.

--- cursos-en-linea/seed.sql
+++ cursos-en-linea/seed.sql
@@ -2,41 +2,40 @@ CREATE DATABASE IF NOT EXISTS cursos_enlinea;
 
 USE cursos_enlinea;
 
-CREATE TABLE IF NOT EXISTS estudiantes (
-	id int not null auto_increment,
-	nombre varchar(50),
-	curso_id int,
-	primary key (id),
-	foreign key (curso_id) references cursos (id)
-)
-
 CREATE TABLE IF NOT EXISTS cursos (
-	id int not null auto_increment,
-	nombre varchar(50),
-	primary key(id)
-)
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	nombre VARCHAR(50)
+);
+
+CREATE TABLE IF NOT EXISTS estudiantes (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	nombre VARCHAR(50),
+	curso_id INT,
+	FOREIGN KEY (curso_id) REFERENCES cursos (id)
+);
 
 
-INSERT INTO cursos (nombre) VALUES ('React') -- 1
-INSERT INTO cursos (nombre) VALUES ('Android') -- 2
-INSERT INTO cursos (nombre) VALUES ('Scrum') -- 3
-INSERT INTO cursos (nombre) VALUES ('iOS')  -- 4
-INSERT INTO cursos (nombre) VALUES ('Java')  -- 5
-INSERT INTO cursos (nombre) VALUES ('Arduino') -- 6
+
+INSERT INTO cursos (nombre) VALUES ('React'); -- 1
+INSERT INTO cursos (nombre) VALUES ('Android'); -- 2
+INSERT INTO cursos (nombre) VALUES ('Scrum'); -- 3
+INSERT INTO cursos (nombre) VALUES ('iOS');  -- 4
+INSERT INTO cursos (nombre) VALUES ('Java');  -- 5
+INSERT INTO cursos (nombre) VALUES ('Arduino'); -- 6
 
 
-INSERT INTO estudiantes (nombre, curso_id) VALUES ('Erick', 2)
-INSERT INTO estudiantes (nombre, curso_id) VALUES ('Erick', 6)
-INSERT INTO estudiantes (nombre, curso_id) VALUES ('Roberto', 1)
-INSERT INTO estudiantes (nombre, curso_id) VALUES ('Roberto', 3)
-INSERT INTO estudiantes (nombre, curso_id) VALUES ('Luis', 3)
-INSERT INTO estudiantes (nombre, curso_id) VALUES ('Ernesto', 1)
-INSERT INTO estudiantes (nombre, curso_id) VALUES ('Ernesto', 2)
-INSERT INTO estudiantes (nombre, curso_id) VALUES ('Ernesto', 3)
-INSERT INTO estudiantes (nombre, curso_id) VALUES ('Ernesto', 4)
-INSERT INTO estudiantes (nombre, curso_id) VALUES ('Ernesto', 5)
-INSERT INTO estudiantes (nombre, curso_id) VALUES ('Ernesto', 6)
-INSERT INTO estudiantes (nombre, curso_id) VALUES ('Carolina', null)
+INSERT INTO estudiantes (nombre, curso_id) VALUES ('Erick', 2);
+INSERT INTO estudiantes (nombre, curso_id) VALUES ('Erick', 6);
+INSERT INTO estudiantes (nombre, curso_id) VALUES ('Roberto', 1);
+INSERT INTO estudiantes (nombre, curso_id) VALUES ('Roberto', 3);
+INSERT INTO estudiantes (nombre, curso_id) VALUES ('Luis', 3);
+INSERT INTO estudiantes (nombre, curso_id) VALUES ('Ernesto', 1);
+INSERT INTO estudiantes (nombre, curso_id) VALUES ('Ernesto', 2);
+INSERT INTO estudiantes (nombre, curso_id) VALUES ('Ernesto', 3);
+INSERT INTO estudiantes (nombre, curso_id) VALUES ('Ernesto', 4);
+INSERT INTO estudiantes (nombre, curso_id) VALUES ('Ernesto', 5);
+INSERT INTO estudiantes (nombre, curso_id) VALUES ('Ernesto', 6);
+INSERT INTO estudiantes (nombre, curso_id) VALUES ('Carolina', null);
 
 -- INNER JOIN
 SELECT estudiantes.nombre, cursos.nombre
